8ffe852f570accde2dfa716f139208a92b482e41
SoDM123 <ngocsopham@gmail.com>
2024-04-08 09:16:05 +0700

    Update Login.java

--- src/loginandsignup/Login.java
+++ src/loginandsignup/Login.java
@@ -1,6 +1,10 @@
 
 package loginandsignup;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends javax.swing.JFrame {
 
@@ -21,13 +25,13 @@ public class Login extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         Left = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
+        userNameLabel = new javax.swing.JLabel();
+        userNameField = new javax.swing.JTextField();
+        passwordLable = new javax.swing.JLabel();
         jPasswordField1 = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        buttoLogin = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        buttonSigup = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("LOGIN");
@@ -90,32 +94,35 @@ public class Login extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(0, 102, 102));
         jLabel1.setText("LOGIN");
 
-        jLabel2.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel2.setText("Email");
+        userNameLabel.setBackground(new java.awt.Color(102, 102, 102));
+        userNameLabel.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        userNameLabel.setText("Username:");
 
-        jTextField1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jTextField1.setForeground(new java.awt.Color(102, 102, 102));
+        userNameField.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        userNameField.setForeground(new java.awt.Color(102, 102, 102));
 
-        jLabel3.setBackground(new java.awt.Color(102, 102, 102));
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel3.setText("Password");
+        passwordLable.setBackground(new java.awt.Color(102, 102, 102));
+        passwordLable.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        passwordLable.setText("Password:");
 
-        jButton1.setBackground(new java.awt.Color(0, 102, 102));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Login");
+        buttoLogin.setBackground(new java.awt.Color(0, 102, 102));
+        buttoLogin.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        buttoLogin.setForeground(new java.awt.Color(255, 255, 255));
+        buttoLogin.setText("Login");
 
         jLabel4.setText("I don't have an account");
 
-        jButton2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(255, 51, 51));
-        jButton2.setText("Sign Up");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        buttonSigup.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        buttonSigup.setForeground(new java.awt.Color(255, 51, 51));
+        buttonSigup.setText("Sign Up");
+        
+        buttonSigup.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+            	jButtonSigupClicked(evt);
             }
         });
+        
+        buttoLogin.addActionListener(e -> jButtonLoginClicked(e));
 
         javax.swing.GroupLayout LeftLayout = new javax.swing.GroupLayout(Left);
         Left.setLayout(LeftLayout);
@@ -130,15 +137,15 @@ public class Login extends javax.swing.JFrame {
                         .addGap(30, 30, 30)
                         .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jLabel2)
-                                .addComponent(jTextField1)
-                                .addComponent(jLabel3)
+                                .addComponent(userNameLabel)
+                                .addComponent(userNameField)
+                                .addComponent(passwordLable)
                                 .addComponent(jPasswordField1, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE)
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(buttoLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(LeftLayout.createSequentialGroup()
                                 .addComponent(jLabel4)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)))))
+                                .addComponent(buttonSigup)))))
                 .addContainerGap(27, Short.MAX_VALUE))
         );
         LeftLayout.setVerticalGroup(
@@ -147,19 +154,19 @@ public class Login extends javax.swing.JFrame {
                 .addGap(51, 51, 51)
                 .addComponent(jLabel1)
                 .addGap(40, 40, 40)
-                .addComponent(jLabel2)
+                .addComponent(userNameLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(userNameField, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel3)
+                .addComponent(passwordLable)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(26, 26, 26)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(buttoLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(33, 33, 33)
                 .addGroup(LeftLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jButton2))
+                    .addComponent(buttonSigup))
                 .addContainerGap(77, Short.MAX_VALUE))
         );
 
@@ -186,7 +193,7 @@ public class Login extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButtonSigupClicked(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         
         SignUp SignUpFrame = new SignUp();
         SignUpFrame.setVisible(true);
@@ -194,7 +201,43 @@ public class Login extends javax.swing.JFrame {
         SignUpFrame.setLocationRelativeTo(null); 
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+    
+    private void jButtonLoginClicked(ActionEvent e)
+    {
+    	String username = userNameField.getText();
+        String password = new String(jPasswordField1.getPassword());
+		try {
+    		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    		String dbUrl = "jdbc:sqlserver://LAPTOP-4DA46HIV:1433; DatabaseName=BaiTap1;encrypt=true;trustServerCertificate=true";
+    		String usernameData = "sa"; 
+    		String passwordData= "123";
+    		Connection connection  = DriverManager.getConnection(dbUrl, usernameData, passwordData);
+    		String insertQuery = "SELECT * FROM Users WHERE Username=? AND Password=?";
+    		PreparedStatement statement = connection.prepareStatement(insertQuery);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                String helloMessage = "Hello " + username;
+                dispose(); // Đóng cửa sổ đăng nhập
+                //Home home = new Home("username");
+            } else {
+                JOptionPane.showMessageDialog(this, "Invalid username or password");
+            }
 
+            resultSet.close();
+            statement.close();
+            connection.close();
+            
+		}catch(Exception ex)
+    	{
+    		ex.printStackTrace();
+    	}
+    }
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -203,17 +246,17 @@ public class Login extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Left;
     private javax.swing.JPanel Right;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton buttoLogin;
+    private javax.swing.JButton buttonSigup;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel userNameLabel;
+    private javax.swing.JLabel passwordLable;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField userNameField;
     // End of variables declaration//GEN-END:variables
 }
